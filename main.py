3b73a3f931fcf83ee255d17ce3b5f63e2c91c5ec
grawish <41504211+grawish@users.noreply.github.com>
2021-05-02 23:40:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import psycopg2
 import json
 
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
 from time import sleep, time, strftime
 
 # set up options for the chromedriver
